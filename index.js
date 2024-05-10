8eb078e8783c3cb46a798f35b95a8f6e1010ae42
ftgyhwudj <gs1kathy795@hotmail.com>
2024-05-10 21:54:37 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-function mergeTwoLists(l1, l2) {
-  if (!l1) return l2;
-  if (!l2) return l1;
-  if (l1.val < l2.val) {
-    l1.next = mergeTwoLists(l1.next, l2);
-    return l1;
-  } else {
-    l2.next = mergeTwoLists(l1, l2.next);
-    return l2;
+function getIntersectionNode(headA, headB) {
+  let currA = headA;
+  let currB = headB;
+  while (currA !== currB) {
+    currA = currA ? currA.next : headB;
+    currB = currB ? currB.next : headA;
   }
+  return currA;
 }

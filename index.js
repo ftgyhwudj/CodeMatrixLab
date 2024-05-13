f63cc46097c2d2318dc9cffb1d537bc7f55c5fb8
ftgyhwudj <gs1kathy795@hotmail.com>
2024-05-13 10:10:55 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-function canPartition(nums) {
-  const sum = nums.reduce((acc, val) => acc + val, 0);
-  if (sum % 2 !== 0) return false;
-  const target = sum / 2;
-  const dp = new Array(target + 1).fill(false);
-  dp[0] = true;
-  for (const num of nums) {
-    for (let i = target; i >= num; i--) {
-      dp[i] = dp[i] || dp[i - num];
-    }
-  }
-  return dp[target];
-}
+const difference = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => !s.has(x));
+};
